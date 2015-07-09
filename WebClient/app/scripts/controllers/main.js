8341328de19179350a8a23e71a507d772450c6e0
Bruno Silva <mogo.bruno@email.com>
2015-07-08 22:04:50 -0300

    layout add no angular

--- WebClient/app/scripts/controllers/main.js
+++ WebClient/app/scripts/controllers/main.js
@@ -8,10 +8,6 @@
  * Controller of the webClientApp
  */
 angular.module('webClientApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+    
   });
