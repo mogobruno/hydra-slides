015342d5813e0da5ea827154d12deb3abcc345c0
Bruno Silva <mogo.bruno@email.com>
2015-07-16 02:28:30 -0300

    add autor na exibição do slide

--- WebClient/app/scripts/controllers/navbarcontroller.js
+++ WebClient/app/scripts/controllers/navbarcontroller.js
@@ -9,7 +9,15 @@
  */
 angular.module('webClientApp')
   .controller('NavbarcontrollerCtrl', function ($scope, requisition, $window) {
-  
+      var user = localStorage.user;
+      if(user !== "undefined"){
+          $scope.hasUser = true;
+          $window.location.href = '#/home';
+      }else{
+          $scope.hasUser = false;
+          $window.location.href = '#/';
+      }
+
       $scope.menu = 1;
 
   		$scope.login = function(user){
